c5dd3d84e05d59438c18210f68fdadc75627d38a
zhangxiaobin <zhangxiaobin@gelaxy.io>
2023-12-05 18:45:23 +0800

    add esc service weekly and monthly data

--- esc/processer.go
+++ esc/processer.go
@@ -15,33 +15,6 @@ func Process(ctx context.Context, days, startHour uint32) *common.Activation {
 	}
 
 	service.Start()
-	
-	// todo get ela block and transactions
-	// 2023-10-01  100
-	// 2023-10-02  200
-	// 2023-10-03  300
-	oneDayTransactionsCount := make(map[string]int)
-	dailyTransactionsCount := make(map[string]int)
-	weeklyTransactionsCount := make(map[string]int)
-	monthlyTransactionsCount := make(map[string]int)
 
-	oneDayActiveAddressesCount := make(map[string]int)
-	dailyActiveAddressesCount := make(map[string]int)
-	weeklyActiveAddressesCount := make(map[string]int)
-	monthlyActiveAddressesCount := make(map[string]int)
-
-	// todo get esc height
-
-	// todo get esc block and transactions
-
-	return &common.Activation{
-		OneDayTransactionsCount:     oneDayTransactionsCount,
-		DailyTransactionsCount:      dailyTransactionsCount,
-		WeeklyTransactionsCount:     weeklyTransactionsCount,
-		MonthlyTransactionsCount:    monthlyTransactionsCount,
-		OneDayActiveAddressesCount:  oneDayActiveAddressesCount,
-		DailyActiveAddressesCount:   dailyActiveAddressesCount,
-		WeeklyActiveAddressesCount:  weeklyActiveAddressesCount,
-		MonthlyActiveAddressesCount: monthlyActiveAddressesCount,
-	}
+	return service.activation
 }
